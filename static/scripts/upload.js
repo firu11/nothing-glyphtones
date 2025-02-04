f9283ded9f55bc9d3e9f8315993e8937e6fae04e
Firu115 <firu@post.cz>
2025-02-04 16:04:08 +0100

    remade the upload circle

--- static/scripts/upload.js
+++ static/scripts/upload.js
@@ -1,15 +1,23 @@
-let form, fileInputElement, fileInputImageElement, fileInputSpanElement
+import WaveSurfer from "/static/scripts/wavesurfer.esm.js"
+
+const images = ["/static/icons/play.svg", "/static/icons/pause.svg", "/static/icons/loading.svg"]
+const imagesRed = ["/static/icons/play-red.svg", "/static/icons/pause-red.svg", "/static/icons/loading.svg"]
+
+let form, fileInputElement, fileInputImageElement, fileInputSpanElement, fileInputAudioPreviewElement, fileInputAudioPlayElement, wave
+let wavesurfer
 
 function handleFile(e) {
     let file = e.srcElement.files[0]
     if (file == undefined) {
         fileInputImageElement.style.display = "block"
-        fileInputSpanElement.style.display = "none"
+        fileInputSpanElement.innerText = "Click to choose a file."
+        fileInputAudioPreviewElement.style.display = "none"
     } else {
         fileInputSpanElement.innerText = file.name
-
         fileInputImageElement.style.display = "none"
-        fileInputSpanElement.style.display = "block"
+        fileInputAudioPreviewElement.style.display = "flex"
+
+        audio(file)
     }
 }
 
@@ -18,19 +26,63 @@ function formChange(e) {
     if (errorMsg !== null) errorMsg.remove()
 }
 
+function audio(file) {
+    if (wavesurfer !== undefined) wavesurfer.destroy()
+    wavesurfer = WaveSurfer.create({
+        container: wave,
+        waveColor: 'white',
+        progressColor: 'red',
+        url: URL.createObjectURL(file),
+        barWidth: 4,
+        barGap: 4,
+        barRadius: 100,
+        cursorWidth: 2,
+        dragToSeek: true,
+        height: 'auto',
+        normalize: true,
+    })
+
+    wavesurfer.on("ready", () => {
+        fileInputAudioPreviewElement.querySelector("button img.white").src = images[0]
+        fileInputAudioPreviewElement.querySelector("button img.red").src = imagesRed[0]
+    })
+
+    wavesurfer.on("finish", () => {
+        fileInputAudioPreviewElement.querySelector("button img.white").src = images[0]
+        fileInputAudioPreviewElement.querySelector("button img.red").src = imagesRed[0]
+    })
+}
+
+function click(e) {
+    if (e.target.firstChild.getAttribute("src") == images[2]) return
+
+    if (wavesurfer.isPlaying()) {
+        wavesurfer.pause()
+        e.target.querySelector(".white").src = images[0]
+        e.target.querySelector(".red").src = imagesRed[0]
+    } else {
+        wavesurfer.play()
+        e.target.querySelector(".white").src = images[1]
+        e.target.querySelector(".red").src = imagesRed[1]
+    }
+}
+
 function reloadSelectors() {
     form = document.querySelector("form#upload")
     if (form == null) {
         return
     }
-    
+
     fileInputElement = form.querySelector("input[type='file']")
-    fileInputImageElement = form.querySelector("img")
+    fileInputImageElement = form.querySelector("#image")
     fileInputSpanElement = form.querySelector("span")
+    fileInputAudioPreviewElement = form.querySelector("#audio")
+    fileInputAudioPlayElement = fileInputAudioPreviewElement.querySelector("button")
+    wave = fileInputAudioPreviewElement.querySelector(".wave")
 
     form.addEventListener("change", formChange)
     fileInputElement.addEventListener("change", handleFile)
-
+    fileInputAudioPlayElement.addEventListener("click", click)
 }
 
 reloadSelectors()
