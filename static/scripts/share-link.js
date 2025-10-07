80a7c48e29c4cb9186edb40e3e8e767cab5c674f
firu11 <firu@firu.dev>
2025-10-07 17:39:06 +0200

    do not show alert when share just canceled

--- static/scripts/share-link.js
+++ static/scripts/share-link.js
@@ -7,7 +7,8 @@ document.querySelector('.share-btn').addEventListener('click', async () => {
             url,
         })
     } catch (err) {
-        alert("Whoops, share canceled or failed. Your browser might not support this feature.\n\nYou can just copy the link in your adress bar and send it yourself.")
+        if (err != "AbortError: Share canceled")
+            alert("Whoops, share failed. Your browser might not support this feature.\n\nYou can just copy the link in your adress bar and send it yourself.")
         console.log("Share canceled or failed: " + err)
     }
 })
