fc04d589f9e9c31ffc9c4dc019b0d1cb603d3c73
firu11 <firu@firu.dev>
2025-11-16 14:31:17 +0100

    insta refresh after search clear

--- static/scripts/filters-dropdown.js
+++ static/scripts/filters-dropdown.js
@@ -28,6 +28,7 @@ function updateHiddenInputs() {
 function enableAndDisableSort() {
     if (search.value === "") {
         sort.disabled = false
+        htmx.trigger(search, "clear-search")
     } else {
         sort.disabled = true
     }
